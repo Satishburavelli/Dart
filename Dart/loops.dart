350dc0f01a972132b7d8c934edd60ca144d88256
Satishburavelli <138653813+Satishburavelli@users.noreply.github.com>
2024-12-30 15:54:18 +0100

    Add files via upload

---
+++ Dart/loops.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  final string= "sk";
+  const pi=3.14;
+  var i =12;
+  if (i%2==0)
+  {
+    print("Number is even: $i");
+  }
+  else
+  {
+    print("Number is odd: $i");
+  }
+  print("final $string:");
+  print("const $pi:");
+}
